34b93ab65ec118cfddd0163557517fc03a487e78
Juby210 <31005896+Juby210@users.noreply.github.com>
2025-02-13 00:23:03 +0100

    bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.juby210.recentsgrid"
         minSdk = 33
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
